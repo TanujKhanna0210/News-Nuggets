536063f08c46a5971d480138a68778f6da79014e
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-20 21:58:02 +0530

    Dependency management modular approach

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,12 @@
+object Versions {
+
+    const val coreKtx = "1.9.0"
+    const val appcompat = "1.6.1"
+    const val material = "1.9.0"
+    const val lifecycleRuntimeKtx = "2.6.2"
+    const val activityCompose = "1.8.1"
+    const val compose = "2023.03.00"
+    const val navigationCompose = "2.7.6"
+    const val lifecycleViewModelKtx = "2.6.2"
+
+}
