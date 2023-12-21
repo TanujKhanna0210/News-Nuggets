5f889aeae2f4d90f5f32a9679b3f5c55ad12ad71
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-22 01:14:43 +0530

    Custom Splash Screen

--- app/src/main/java/com/example/newsnuggets/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsnuggets/ui/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.newsnuggets.ui.navigation.AppNavigationGraph
 import com.example.newsnuggets.ui.theme.NewsNuggetsTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NewsNuggetsTheme {
                 Surface(
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -34,6 +34,8 @@ object Dependencies {
     val coroutinesCore by lazy { "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}" }
     val coroutinesAndroid by lazy { "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}" }
 
+    val splashScreen by lazy { "androidx.core:core-splashscreen:${Versions.splashScreen}" }
+
 }
 
 object Modules {
