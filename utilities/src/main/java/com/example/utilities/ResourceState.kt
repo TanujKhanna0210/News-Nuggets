e139d3427ae5ee1080fe874db3216eb24264ccf2
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-21 21:21:22 +0530

    Flow creation for api response

--- utilities/src/main/java/com/example/utilities/ResourceState.kt
+++ utilities/src/main/java/com/example/utilities/ResourceState.kt
@@ -4,6 +4,6 @@ sealed class ResourceState<T> {
 
     class Loading<T> : ResourceState<T>()
     data class Success<T>(val data: T) : ResourceState<T>()
-    data class Error<T>(val error: T) : ResourceState<T>()
+    data class Error<T>(val error: String) : ResourceState<T>()
 
 }
